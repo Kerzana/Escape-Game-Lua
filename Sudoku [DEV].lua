30b9e20fd52a5e2df5a2272e233fd9ff8fb6b417
Kerzana <33467099+Kerzana@users.noreply.github.com>
2017-12-12 16:16:06 +0100

    Update Sudoku [DEV].lua

--- Sudoku [DEV].lua
+++ Sudoku [DEV].lua
@@ -1,4 +1,4 @@
-function checkColumn(tableau, line, column)
+function checkColumn(tableau,line,column)
     errror = 0
     for linecheck = 1, 9 do
         if tableau[linecheck][column] ~= "-" then
@@ -13,7 +13,7 @@ function checkColumn(tableau, line, column)
     return true
 end
 
-function checkLine(tableau, line, column)
+function checkLine(tableau,line,column)
     failure = 0
     for checkcolumn = 1, 9 do
         if tableau[line][checkcolumn] ~= "-" then
@@ -28,10 +28,10 @@ function checkLine(tableau, line, column)
     return true
 end
 
-function checkBox(tableau, line, column)
+function checkBox(tableau,line,column)
     minLine = 0
     maxline = 0
-    if line >= 1 and line <= 3 then
+    if  line >= 1 and line <= 3 then
         minLine = 1
         maxline = 3
     else if line >= 4 and line <= 6 then
@@ -46,7 +46,7 @@ function checkBox(tableau, line, column)
     end
     mincolumn = 0
     maxcolumn = 0
-    if column >= 1 and column <= 3 then
+    if  column >= 1 and column <= 3 then
         mincolumn = 1
         maxcolumn = 3
     else if column >= 4 and column <= 6 then
@@ -75,38 +75,41 @@ function checkBox(tableau, line, column)
     return true
 end
 
-function checkValid(tableau, i, j)
-    return (checkBox(tableau, i, j) and checkColumn(tableau, i, j) and checkLine(tableau, i, j))
+function checkValid(tableau,i,j)
+    return(checkBox(tableau,i,j) and checkColumn(tableau,i,j) and checkLine(tableau,i,j))
 end
 
 function printtableau(tableau)
-    term.setCursorPos(1, 1)
-    x = 0
-    for w = 1, 9 do
-        term.setCursorPos(1, x)
-        if w == 4 or w == 7 then
-            term.setTextColor(colors.orange)
-        else
-            term.setTextColor(colors.blue)
-        end
-        term.write("------------------")
-        term.setTextColor(colors.white)
-        term.setCursorPos(1, x + 1)
-        for z = 1, 9 do
-            term.write(tableau[w][z])
-            if z == 3 or z == 6 then
-                term.setTextColor(colors.orange)
-            else
-                term.setTextColor(colors.blue)
-            end
-            term.write("|")
-            term.setTextColor(colors.white)
-        end
-        x = x + 2
+term.setCursorPos(1,1)
+x = 0
+for w = 1, 9 do
+    term.setCursorPos(1,x)
+    if w == 4 or w == 7 then
+    	term.setTextColor(colors.red)
+    else
+    	term.setTextColor(colors.gray)
     end
+    term.write("------------------")
+    term.setTextColor(colors.yellow)
+    term.setCursorPos(1,x + 1)
+    for z = 1, 9 do
+    	if tableau[w][z] == "-" then
+    		term.setTextColor(colors.lightGray)
+    	end
+        	term.write(tableau[w][z])
+        	if z == 3 or z == 6 then
+    			term.setTextColor(colors.red)
+    		else
+    			term.setTextColor(colors.gray)
+    		end
+    	term.write("|")
+    	term.setTextColor(colors.yellow)
+    end
+    x = x + 2
+end
 end
 
-function resolvSudoku(tableau, line, column)
+function resolvSudoku(tableau,line,column)
     tmpre = 0
     for line = line, 9 do
         if tmpre == 2 then
@@ -118,13 +121,13 @@ function resolvSudoku(tableau, line, column)
                 for case = 1, 9 do
                     tableau[line][column] = case
                     --printtableau(tableau)
-                    tmp = checkValid(tableau, line, column)
-                    if tmp == true then
-                        relosv = resolvSudoku(tableau, line, column + 1)
+                    tmp = checkValid(tableau,line,column)
+                    if  tmp == true then
+                    	relosv = resolvSudoku(tableau,line,column+1)
                         if relosv == true then
                             return false
                         else if relosv == tableau then
-                            return tableau
+                        	return tableau
                         end
                         end
                     end
@@ -143,43 +146,67 @@ end
 
 function sudokutotable(arg)
     tableau = {
-        [0] = { [1] = "-", [2] = "-", [3] = "-", [4] = "-", [5] = "-", [6] = "-", [7] = "-", [8] = "-", [9] = "-" }
+        [0] = {[1]="-", [2]="-", [3]="-", [4]="-", [5]="-", [6]="-", [7]="-", [8]="-", [9]="-"}
     }
 
     for y = 1, 9 do
         tableau[y] = {}
         for i = 1, 10 do
-            table.insert(tableau[y], i, string.char(string.byte(arg, i)))
+            table.insert(tableau[y],i,string.char(string.byte(arg, i)))
         end
-        arg = string.sub(arg, 11, arg:len())
+        arg = string.sub(arg,11,arg:len())
     end
 
     for n = 1, 9 do
         for b = 1, 9 do
-            if checkValid(tableau, n, b) then
+            if checkValid(tableau,n,b) then
             else
                 return false
             end
         end
     end
-
-    finishtableau = resolvSudoku(tableau, 1, 1)
+	printtableau(tableau)
+    finishtableau = resolvSudoku(tableau,1,1)
     return finishtableau
 end
 
-
-
-term.clear()
-term.setCursorPos(1, 1)
-
-blue = sudokutotable("1----7-9-\n-3--2---8\n--96--5--\n--53--9--\n-1--8---2\n6----4---\n3------1-\n-4------7\n--7---3--\n")
-
-if blue == false then
-    term.clear()
-    term.setCursorPos(1, 1)
-    term.write("Error No Valid Sudoku.")
-    return false
+function sudoku()
+	term.clear()
+	term.setCursorPos(1,1)
+	blue = sudokutotable("1----7-9-\n-3--2---8\n--96--5--\n--53--9--\n-1--8---2\n6----4---\n3------1-\n-4------7\n--7---3--\n")
+	if blue == false then
+	    term.clear()
+	    term.setCursorPos(1,1)
+	    term.write("Error No Valid Sudoku.")
+	    return false
+	end
+	term.clear()
+	printtableau(blue)
 end
 
-term.clear()
-printtableau(blue)
+function timer()
+	x = 0
+	y = 0
+	z = 0
+	w = 1
+	while w do
+		term.setCursorPos(28,18)
+		x = x + 1
+		if x >= 60 then
+			x = 0
+			y = y + 1
+		end
+		if y >= 60 then
+			y = 0
+			z = y + 1
+		end
+		if z == 60 then
+			w = 0
+		end
+		term.write(z.."h:"..y.."m:"..x.."s")
+		sleep(1)
+	end
+	return true
+end
+
+re = parallel.waitForAll(sudoku,timer)
