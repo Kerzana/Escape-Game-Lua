d506f9180ace7fb9ef1ea54d770538eaf810ed85
Kerzana <33467099+Kerzana@users.noreply.github.com>
2017-12-13 15:38:51 +0100

    Update Sudoku Play.lua

--- Sudoku Play.lua
+++ Sudoku Play.lua
@@ -79,7 +79,7 @@ function checkValid(tableau,i,j)
     return(checkBox(tableau,i,j) and checkColumn(tableau,i,j) and checkLine(tableau,i,j))
 end
 
-function printtableau(tableau,x,y)
+function printtableau(tableau)
 	l = 0
 	term.setCursorPos(1,1)
 	x = 1
@@ -149,13 +149,33 @@ function readfile()
 	return tableau
 end
 
+function readfilesave()
+	tableau = {
+		[0] = {}
+	}
+	local h = fs.open("tableausave","r")
+	for	b = 1, 9 do
+		tableau[b] = {}
+		for a = 1, 9 do
+			line = h.readLine()
+			line = string.sub(line,0,1)
+			table.insert(tableau[b],a,line)
+		end
+	end
+	h.close()
+	return tableau
+end
+
 function clickEvent(tableau,x,y)
 	x = math.floor(x/2)
 	y = math.floor(y/2)
+
 	if x < 10 and y < 10 and x > 0 and y > 0 then
 		if tableau[y][x] == "-" then
 			return true
 		end
+	else
+		return exitsudoku
 	end
 	return false
 end
@@ -163,23 +183,47 @@ end
 function start()
 	term.clear()
 	term.setCursorPos(1,1)
-	tableau = readfile()
+	list = shell.programs()
+	for k = 0, #list do
+		if list[k] == "tableausave" then
+			tableau = readfilesave()
+			m = 0
+			break
+		end
+		m = 1
+	end
+	if m == 1 then
+		tableau = readfile()
+	end
 	worktableau = tableau
 	while true do
 		x = 2
 		y = 2
 		term.clear()
-		fini = printtableau(worktableau,x,y)
+		fini = printtableau(worktableau)
 		if fini then
 			break
 		end
 		local event, button, x, y = os.pullEvent( "mouse_click" )
-		if clickEvent(readfile(),x,y) then
+		if clickEvent(readfile(),x,y) == true then
 			term.setCursorPos(math.floor(x/2)*2,math.floor(y/2)*2)
 			tmp = read()
 			if string.len(tmp) == 1 then
 				worktableau[math.floor(y/2)][math.floor(x/2)] = tonumber(tmp)
 			end
+		elseif clickEvent(readfile(),x,y) == exitsudoku then
+			term.setCursorPos(25,5)
+			term.write("Exit Programme.")
+			local h = fs.open("tableausave","w")
+			for i = 1, 9 do
+				for j = 1, 9 do
+					h.write(worktableau[i][j])
+					h.write("\n")
+				end
+			end
+			h.close()
+			sleep(3)
+			break
 		end
 	end
 	return false
